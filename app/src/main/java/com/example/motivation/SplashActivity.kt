2c5763b675efa15a3026c376113616ad25063a78
priscilla <priscillasilva-24@hotmail.com>
2021-08-22 19:24:14 -0300

    create infra package

--- app/src/main/java/com/example/motivation/SplashActivity.kt
+++ app/src/main/java/com/example/motivation/SplashActivity.kt
@@ -1,19 +1,29 @@
 package com.example.motivation
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import com.example.motivation.infra.SecurityPreferences
 import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var mSecurityPreferences: SecurityPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        mSecurityPreferences = SecurityPreferences(this)
+
         //atribuindo o evento de clique do botão Salvar
         buttonSave.setOnClickListener(this)
+
+        val securityPreferences = SecurityPreferences(this)
+        securityPreferences.storeString("", "")
     }
 
     // tratando qual o botão que foi clicado
@@ -29,6 +39,7 @@ class SplashActivity : AppCompatActivity(), View.OnClickListener {
         val name = editName.text.toString()
 
         if (name != "") {
+            mSecurityPreferences.storeString("name", name)
             startActivity(Intent(this, MainActivity::class.java))
         } else {
             Toast.makeText(this, "Informe seu nome", Toast.LENGTH_SHORT).show()
